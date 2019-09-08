be00b20cd2a6a579f6e9bb88219b4957b93cfbbe
Michał Lisowski <michael.lisowski@zoho.eu>
2019-09-08 14:18:00 +0200

    Added POST, PUT and DELETE requests

--- src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicController.java
@@ -1,10 +1,8 @@
 package io.javabrains.springbootstarter.topic;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController //Annotation provides auto parsing to JSON for every requests
@@ -13,8 +11,28 @@ public class TopicController {
 	@Autowired
 	private TopicService topicService;
 
-	@RequestMapping("/topics")
+	@RequestMapping("/topics")  // Default request - GET
 	public List<Topic> getAllTopics() {
 		return topicService.getAllTopics();
+	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
 	}
 }
--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,19 +2,42 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service    //Singleton
 public class TopicService {
-
-	private List<Topic> topics = Arrays.asList(
+	// Arrays.asList creates immutable object
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Desc"),
 			new Topic("java", "Core Java", "Core Java Desc"),
 			new Topic("javascript", "JavaScript", "JavaScript Desc")
-	);
+	));
 
-	public  List<Topic> getAllTopics() {
+	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(topic -> topic.getId().equals(id));
 	}
 }
